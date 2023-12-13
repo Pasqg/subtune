71fb44a2c9750e34ba22e73985989fe6f17f4581
Pasqg <pasquale.giovenale@gmail.com>
2023-12-13 22:56:40 +0100

    Make wavelet transform return complex numbers
    Currently, the transform already returns the modulo of the complex
    convolution. Instead, the transform should return the complex number
    itself, as it is the caller's responsibility to further process the
    trasnform result, for example taking the modulo if it wants to
    visualize the transform.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::time::Instant;
-use crate::signals::sine_signal;
+use crate::signals::{SignalSample, sine_signal};
+use crate::math::{ComplexNum, modulo};
+use crate::wavelets::MORLET_HALF_LENGTH;
 
 mod wavelets;
 mod signals;
@@ -8,16 +10,17 @@ mod math;
 
 fn main() {
     let time = Instant::now();
-    let sample_rate = 512;
+    let sample_rate = 2048;
+    let signal_duration_s = 1;
     let sine_samples =
-        signals::SignalSample::from_signal(3.0, sample_rate, &|t| sine_signal(t, 17.0));
+        signals::SignalSample::from_signal(signal_duration_s as f64, sample_rate, &|t| sine_signal(t, 200.0));
 
+    let frequencies = sample_rate;
     let result = wavelets::transform::wavelet_transform(&sine_samples, &|frequency, sample_rate| {
         let wavelet = wavelets::morlet(frequency);
         signals::SignalSample::from_wavelet(2.0 * MORLET_HALF_LENGTH / frequency, sample_rate, &|x| wavelet(x))
-    }, 1.0, 33.0, 16);
+    }, 1.0, 500.0, frequencies);
 
-    println!("{:?}", result[15]);
     println!("Elapsed {:?}", time.elapsed());
 
 //    save_wav("wavelet440hz3s.wav", &sample);
